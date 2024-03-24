6cbe28e915f519313108ffb61e93ff19afa97d85
rahul-7416 <rj.cu74@gmail.com>
2024-03-24 22:37:46 +0530

    changed the title

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -6,7 +6,7 @@ function WelcomePage() {
     <div className='h-lvh w-lvw flex justify-center items-center p-5 bg-slate-600'>
       <div className='bg-blue-400 h-[50%] w-[90%] md:h-[50%] md:w-[50%] flex flex-col justify-around rounded-xl px-5 py-0'>
         <div className='flex flex-col gap-6'>
-            <h1 className='self-center font-bold text-4xl '>Welcome to xyz</h1>
+            <h1 className='self-center font-bold text-4xl '>Welcome to CodeInBound</h1>
             <p className='self-center '>Help us improve for the better</p>
             <p className='self-center text-center'>Fill the survey form, and share your valuable feedback to us.</p>
         </div>
